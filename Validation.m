d2427fde95262ff056a39fe92eb32cf399bfb573
Martin <marbla-6@student.ltu.se>
2018-05-09 14:48:22 +0200

    added gitignore

--- Validation.m
+++ Validation.m
@@ -2,14 +2,15 @@
 % capacity for numerical evaluation. Code is taken from the presentation
 % material. 
 
-visc    = 10;                            % Viscosity [Pas] 
+visc    = 10;                           % Viscosity [Pas] 
 U       = 5;                            % Runner speed [m/s]
-L       = 10;                            % Bearing length [m]
-hmin    = 0.0001;                            % Trailing edge film thickness [m]
+L       = 10;                           % Bearing length [m]
+hmin    = 0.0001;                       % Trailing edge film thickness [m]
 k       = linspace(0,1e-3,100);         % Slope parameter 
 x       = 1;                            % Position
 
-tryckUppg = ((6.*visc.*U.*L)./(hmin.^2)); % Tryckuppgyggnadens variation med alla designparametrar utom k.
+% Tryckuppgyggnadens variation med alla designparametrar utom k.
+tryckUppg = ((6.*visc.*U.*L)./(hmin.^2)); 
 
 p = @(x) tryckUppg.*((1./k).*(1./(1+k-k.*x./L))-((1+k)./(2+k)).*(1./(1+k-k.*x./k).^2)-1./(2+k));
 N = log(1+k)./k.^2 - 2./(2+k)./k;
