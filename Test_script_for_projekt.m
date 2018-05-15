4f80d89f895c5561b7afd64adc9c5d8d14a9cf53
Lias Sjölund <lias.sjolund@gmail.com>
2018-05-15 15:30:16 +0200

    hiva in lite grejjer, kanske har koll

--- Test_script_for_projekt.m
+++ Test_script_for_projekt.m
@@ -3,15 +3,16 @@ U       = 5;                            % Runner speed [m/s]
 L       = 10;                           % Bearing length [m]
 hmin    = 0.0001;                       % Trailing edge film thickness [m]
 k       = linspace(0,1e-3,100);         % Slope parameter 
-x1      = 1;                            % Position
-x2      = 0:0.1:10;
-y0      = 0;
+N       = ???;
 
-tryckUppg = ((6.*visc.*U.*L)./(hmin.^2));
-p = @(x) tryckUppg.*((1./k).*(1./(1+k-k.*x./L))-((1+k)./(2+k)).*(1./(1+k-k.*x./k).^2)-1./(2+k));
-y1 = p(x1);
+x(i) = i.*L./N;
+h = hmin(1+k-(k./L));
+a = h.^3/(12.*visc);
+F = U/2;
 
-func = @(x,h) (6*visc*U)/(hmin(1+k-(k/L)*x));
-%y2 = rungekutta4(func,x2,y0);
-
-plot(k,y1);
+c(i) = (a(i-1)+a(i))/(2.*deltax.^2);
+d(i) = -(a(i-1)+2.*a(i)+a(i+1))/(2.*deltax.^2);
+e(i) = (a(i)+a(i+i))/(2.*deltax.^2);
+z(i) = (F(i+1)-F(i-1))/(2.*deltax);
+g(i) = -(F(i)/deltax);
+k(i) = F(i-1)/deltax;
