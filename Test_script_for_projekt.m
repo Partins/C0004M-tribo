e165dd99e9abb249fe4069c81dfc76545f23d562
Lias Sjölund <lias.sjolund@gmail.com>
2018-05-16 11:38:33 +0200

    typ fungerande nu

--- Test_script_for_projekt.m
+++ Test_script_for_projekt.m
@@ -3,16 +3,25 @@ U       = 5;                            % Runner speed [m/s]
 L       = 10;                           % Bearing length [m]
 hmin    = 0.0001;                       % Trailing edge film thickness [m]
 k       = linspace(0,1e-3,100);         % Slope parameter 
-N       = ???;
+%k        = 50;
+N       = 10;
+u(1)    = 0;
 
-x(i) = i.*L./N;
-h = hmin(1+k-(k./L));
-a = h.^3/(12.*visc);
-F = U/2;
+h = hmin.*(1+k-(k./L));
+a = (h.^3)./(12.*visc);
+F = (U/2).*h;
+deltax = L/N;
+    
+    for i = 2:N-1
+        x(i) = i.*L./N;
 
-c(i) = (a(i-1)+a(i))/(2.*deltax.^2);
-d(i) = -(a(i-1)+2.*a(i)+a(i+1))/(2.*deltax.^2);
-e(i) = (a(i)+a(i+i))/(2.*deltax.^2);
-z(i) = (F(i+1)-F(i-1))/(2.*deltax);
-g(i) = -(F(i)/deltax);
-k(i) = F(i-1)/deltax;
+        c(i) = (a(i-1)+a(i))./(2.*deltax.^2);
+        d(i) = -(a(i-1)+2.*a(i)+a(i+1))/(2.*deltax.^2);
+        e(i) = (a(i)+a(i+1))/(2.*deltax.^2);
+        z(i) = (F(i+1)-F(i-1))./(2.*deltax);
+        g(i) = -(F(i)/deltax);
+        K(i) = F(i-1)/deltax;
+    end
+A = spdiags([c' d' e'],[-1 0 1],N-1,N-1);
+B = spdiags([g' K'],[-1 0],N-1,N-1);
+f = [z(1)-c(1)*u(1) z(2:end-1) z(N-1)-e(N-1)]';
