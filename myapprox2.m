be683d8da09ca3185d6354a5ff28cb74c68c4b0e
Daniel Olsson <admin@Daniels-MBP.lokal.xp-el.com>
2018-05-18 15:08:11 +0200

    tryhard

---
+++ myapprox2.m
@@ -0,0 +1,44 @@
+%% Varible slope parameter k, konstant xs (length of "xi")
+
+%Setup
+N       = 10;                           % Antal obekanta/Antal delar på intervallet.
+mu      = 0.01;                         % Viscosity [Pas] 
+U       = 1;                            % Runner speed [m/s]
+l       = 0.02;                         % Bearing length [m]
+hmin    = 10^-6;                        % Trailing edge film thickness [m]
+k       = linspace(0,1e-3,N);           % Slope parameter
+p0      = 0;                            % left limit
+pL      = 0;                            % right limit
+xs      = l/N                         % steplength
+h       = hmin*(1+k-(k/l));             % vektor h, with length N+1
+
+
+a = (h.^3)/(12*mu); %Längd N
+F = (U/2).*h;
+% X = [0 x:x:l-x]
+
+%% Diagonaler matris A
+
+c1 = (a(1:N-1)+a(2:N))/(2*xs^2); %underdiag
+d1 = -(a(1:N-1)+2*a(2:N)+a(1:N-1))/(2*xs^2); %huvudiag
+e1 = (a(1:N-1)+a(2:N))/(2.*xs.^2); %överdiag
+
+%% Högerled
+z = (F(2:N)-F(1:N-1))/(2*xs);
+
+%% Uppsättning matris A
+
+e = [0 e1(1:end-1)];
+c = [c1(2:end) 0];
+A = spdiags([c' d1' e'],[-1 0 1],N-1,N-1); %Se ekv.31, gles matris A
+
+f = [z(1)-c1(1)*p0 z(2:end-1) z(N-1)-e(N-1)*pL]'; %Se ekv.32, högerled 
+
+%% Lösning av ekvationssystem
+u = A\f;
+
+%% Plot av tryck med varierande k.
+
+p = [p0 u' pL];
+figure
+plot([0 k],p);
